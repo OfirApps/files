931f8b0043fea5e3d48591fa80075875c6ce817c
OfirApps <73336956+OfirApps@users.noreply.github.com>
2023-12-13 21:17:54 +0200

    a

--- ect.js
+++ ect.js
@@ -1,1 +1,11 @@
 console.warn('כל התוכן מוגן בזכויות יוצרים')
+
+var styles = `
+    html {
+        direction:rtl;
+    }
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)

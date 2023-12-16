9fcde1b3ad3f67e5aa2bf5b0c79f8759879daab6
OfirApps <73336956+OfirApps@users.noreply.github.com>
2023-12-16 15:05:39 +0200

    yuh

--- ect_work_on_this.js
+++ ect_work_on_this.js
@@ -4,20 +4,25 @@ function scrolltobodyMainPage() {
     document.querySelector("div#safearea").scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
 }
 
-const elements = document.querySelectorAll("#full > div.cover-body > div.bottom > div > div > a > img");
+// const elements = document.querySelectorAll("#full > div.cover-body > div.bottom > div > div > a > img");
 
-elements.forEach((e, index) => {
- const alt = `מטוס`;
- if (index === 1) {
-   alt = "טלפון סלולרי";
- } else if (index === 2) {
-   alt = "חץ המופנה כלפי מטה";
- } else if (index === 3) {
-   alt = "עיתון";
- } else if (index === 4) {
-   alt = "כוכב";
- }
- e.alt = alt;
- e.width="40px";
- e.height="40px;"
-});
+// elements.forEach((e, index) => {
+// a = `מטוס`;
+//  if (index === 1) {
+//    a = "טלפון סלולרי";
+//  } else if (index === 2) {
+//    a = "חץ המופנה כלפי מטה";
+//  } else if (index === 3) {
+//    a = "עיתון";
+//  } else if (index === 4) {
+//    a = "כוכב";
+//  }
+//  e.a = a;
+// });
+
+
+// for (let i = 1; i <= document.querySelectorAll("a.avatar > img").length; i++) {
+//     e = document.querySelectorAll(`a.avatar > img`)[i];
+//     e.alt = "תמונת פרופיל";
+//     e.parentNode.ariaLabel = "מידע על הכותב";
+// }
